7d06b867eac2410c17071b8f98e6c9d66319f2e2
Frederick Katsura <katsurinstudios@protonmail.ch>
2022-12-04 04:06:54 -0400

    Finished Day 4!

---
+++ src/day4/part2.ts
@@ -0,0 +1,18 @@
+import { readFileSync } from "fs";
+const values = readFileSync("./inputs/Day4.txt", "utf8").split("\r\n");
+
+let count = 0;
+
+for (let i = 0; i < values.length; i++) {
+	const [elf1Min, elf1Max, elf2Min, elf2Max] = values[i]
+		.replace(",", " ")
+		.replace(/-/g, " ")
+		.split(" ", 4)
+		.map((e) => Number(e));
+
+	if (elf1Min <= elf2Min && elf2Min <= elf1Max || elf2Min <= elf1Min && elf1Min <= elf2Max) {
+		count++;
+	}
+}
+
+console.log(count);
